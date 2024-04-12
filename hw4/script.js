de80d5f2e1e251d96ebd68fefdf22eddcd40560c
boykostas <boykostas@gmail.com>
2024-04-12 17:37:12 +0300

    Lesson 7 Test

---
+++ hw4/script.js
@@ -0,0 +1,18 @@
+// let num1 = [5, 7, 8, 3, 5, 1, 0, 9]
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// const number = Number(prompt('Введите число: '));
+// let i = 1;
+// while (i <= number) {
+//     console.log(i++);
+// }
+
+const arr = [6];
+arr.push('Hello');
+arr.push(4);
+arr.push('Jhon')
+console.log(arr[arr.length - 1]);
