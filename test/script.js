509855d6d98275e15459fc0244f1eb16d192ece5
boykostas <boykostas@gmail.com>
2024-04-14 23:46:35 +0300

    Test JS

---
+++ test/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function sum(a, b, c) {
+    console.log(a + b + c);
+}
+
+let param1 = 1;
+let param2 = 6;
+let param3 = 12;
+
+sum(param1, param2, param3)
+
+function click() {
+    let text = prompt('Введите свое имя: ');
+    console.log(text)
+}
