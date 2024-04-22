12cca5c0fa23f39be24c926c22f169f1f0112b4a
boykostas <boykostas@gmail.com>
2024-04-22 12:39:36 +0300

    Test tasks

--- test/script.js
+++ test/script.js
@@ -1,11 +1,31 @@
-'use strict'
+// const p = document.querySelector('p');
+// p.textContent = 'Привет мир!';
 
-function sum(a, b, c) {
-    console.log(a + b + c);
-}
+// const a = document.querySelector('a');
+// a.textContent = 'link text js';
+// a.href = 'https://developer.mozilla.org/ru/';
 
-let param1 = 1;
-let param2 = 6;
-let param3 = 12;
+// const photo = document.querySelector('.photo');
+// photo.src = 'https://klike.net/uploads/posts/2023-02/1675842971_3-353.jpg'
 
-sum(param1, param2, param3);
+// const p3 = document.createElement('p');
+// p3.textContent = 'Новый текстовый элемент';
+
+// const content = document.querySelector('.content');
+// const btn = document.createElement('button');
+// btn.textContent = 'Счётчик';
+// content.appendChild(btn);
+
+// let count = 0;
+// btn.onclick = () => {
+//     count++;
+//     console.log(`Кнопка нажата ${count} раз`);
+// }
+
+// const content = document.querySelector('.content');
+// const btn = document.createElement('button');
+// btn.textContent = 'Отправить';
+// content.appendChild(btn);
+// btn.addEventListener('click', () => {
+//     btn.textContent = 'Текст отправлен';
+// });
