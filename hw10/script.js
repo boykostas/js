84ad96ee220177ae5635348bbb0483c96bf68412
boykostas <boykostas@gmail.com>
2024-06-09 22:39:49 +0300

    Lesson 10 HW10 Сontinuation full

--- hw10/script.js
+++ hw10/script.js
@@ -6,8 +6,6 @@
 // 3. Создать файл data.js.
 // 4. Создать переменную которая будет хранить данные из публичных api.
 
-
-
 // Задание 2
 // 1. Создать файл index.html.
 // 2. Подключить data.js.
@@ -18,7 +16,16 @@
 const newData = JSON.parse(data);
 console.log(newData);
 
-const divContent = document.querySelector('.content');
-newData.forEach(element => {
-    
+const divContent = document.querySelector(".content");
+newData.forEach((element) => {
+  divContent.insertAdjacentHTML(
+    "beforeend", `
+    <div class="wrapper">
+        <h2>${element.id}</h2>
+        <h3>${element.username}</h3>
+        <p>${element.address.street}</p>
+        <a href="tel:+${element.phone}">${element.phone}</a>
+    </div>
+    `
+  );
 });
